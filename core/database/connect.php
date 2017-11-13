205a417ab281c90f3b9c8ec1dd7b63d774251f7b
robertmeans <robert@robertmeans.com>
2017-11-13 12:58:31 -0700

    new connection string
    moved myscreenscene.com into free hosting account at GoDaddy and had to create database with different name.

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,6 +1,6 @@
 <?php
 $connect_error = 'Sorry, there seems to be a connection problem. I think the Internet just hiccuped. Please try again.';
-mysql_connect('localhost', 'homepage80439', 'Ab3kch8a!') or die($connect_error);
-mysql_select_db('homepage80439') or die($connect_error);
+mysql_connect('localhost', 'myhomepage80439', '!072SH%@Ij9%39VUA2yt!pkBb#') or die($connect_error);
+mysql_select_db('myhomepage80439') or die($connect_error);
 
 ?>
