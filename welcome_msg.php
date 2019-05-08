c5199690c65f0512f174900f974621c3d629f6e8
Evergreen Web Design <info@evergreenwebdesign.com>
2019-05-08 17:41:21 -0600

    changed Welcome message

--- welcome_msg.php
+++ welcome_msg.php
@@ -1,9 +1,18 @@
 <img src="images/spacer.gif" border="0" width="1" height="10" /><br />
 <h2>My Homepage Project</h2>
-<p class="youtubeWelcome"><a href="http://www.youtube.com/watch?v=9lueiWHE-sY" class="youtubeLink" target="_blank">Instructional Video</a> - 2<sup>nd</sup> try, a little more conscise.<br />
+
+<p class="welcomeText">If you have a little time on your hands and you want to be mildly entertained while listening to me babble on and on about what, why and how this site is the greatest thing since beer was invented then settle in to the instructional videos below and let the manic enthusiasm begin!<br />
 <br />
-<a href="http://www.youtube.com/watch?v=aXIGH0Jt7X8" class="youtubeLink" target="_blank">Instructional Video</a> - First try... sensory overload.
+Alternatively, you can just jump right in and sort it out on your own as it is incredibly intuitive and genuinely useful.</p>
+
+<p class="welcomeText">
+	<a href="http://www.youtube.com/watch?v=aXIGH0Jt7X8" class="youtubeLink" target="_blank">Instructional Video</a> - First try... sensory overload.<br />
+	<a href="http://www.youtube.com/watch?v=9lueiWHE-sY" class="youtubeLink" target="_blank">Instructional Video</a> - 2<sup>nd</sup> try, a little more conscise.
+
 </p>
+<!-- 
+If you're looking at the source code and reading this that's pretty funny. Who does that? I do. Virtually every website I visit I end up in the source to see how they did something. That aside, the following text is what was originally on this page. I left it here for somene like you. It's the original copy outlining my infintile PHP brainchild that I had so much fun creating. Any rate, I babble... 
+
 <p class="welcomeText">Years and years ago I built a homepage designed with all the features I want available as soon as I open a browser window. Over time as technology evolved, my custom homepage began to fall apart across the various browsers until all I could use it with was Internet Explorer. Oh the horror!<br />
 <br />
 Finally I said, &quot;enough!&quot; and began what I knew would be a time consuming project; rebuilding my beloved homepage. Of course every last detail was a meticulous, pain staking endeavor but I emerged with something I was proud of. So proud in fact that I decided to make it available to everyone.<br />
@@ -13,5 +22,7 @@ MyScreenScene does not try to be all things to all people. It was designed by me
 After you Register an account click on the link at the top, &quot;Edit Mode&quot; and start styling this to suit your needs. Once finished click on &quot;Home&quot; and all your changes will be saved. Make as many changes as often as you'd like. Your page will always be available as you left it.<br />
 <br />
 
-Enjoy!</p>
+Enjoy!</p> 
+
+-->
 <div class="welcomeBob">Sincerely,<br />Bob</div><!-- welcomeBob -->
