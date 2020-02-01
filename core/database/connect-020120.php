b6c9f4bd0ac54c9eedb3cfb731e5ec08f1b05362
Evergreen Web Design <info@evergreenwebdesign.com>
2020-02-01 08:41:44 -0700

    Updated for new (old) server
    changes so it will work on old server after hosting expired from "free" spot under the Reseller program. I bought $12 for 1 year hosting on old server and had to move everything.

--- core/database/connect-020120.php
+++ core/database/connect-020120.php
@@ -1,6 +1,6 @@
 <?php
 $connect_error = 'Sorry, there seems to be a connection problem. I think the Internet just hiccuped. Please try again.';
-mysql_connect('homepage80439.db.14069514.4d9.hostedresource.net', 'homepage80439', 'A!072SH%@Ij9%39VUA2yt!pkBb#') or die($connect_error);
+mysql_connect('localhost', 'homepage80439', 'A!072SH%@Ij9%39VUA2yt!pkBb#') or die($connect_error);
 mysql_select_db('homepage80439') or die($connect_error);
 
 ?>
